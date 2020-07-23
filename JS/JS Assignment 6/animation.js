43734dbba44519e6ed26ae75d6345cbd0f0449f2
Kusum Ranjitkar <kusumranjit78@gmail.com>
2020-07-23 08:18:12 +0545

    JS  assignment 6

---
+++ JS/JS Assignment 6/animation.js
@@ -0,0 +1,40 @@
+function Animate() {
+	var balls = [];
+
+	this.start = function(n) {
+		for (i = 1; i <= n; i++) {
+			var ball = new Ball(i);
+			ball.create();
+			var dimension = parseInt(Math.random() * 80);
+			ball.setDimension(dimension, dimension);
+			var position = new Position(window.innerWidth - dimension, 0);
+			position.generateXY();
+			ball.setPosition(position.x, position.y);
+			balls.push(ball);
+			// ball.move();
+			var timeout = Math.random() * 7 * 1000;
+			setTimeout(
+				(function() {
+					var currentBall = ball;
+					return function() {
+						currentBall.move();
+					};
+				})(),
+				timeout
+			);
+		}
+	};
+
+	this.removeBall = function() {
+		setInterval(function() {
+			balls.forEach(function(ball, index) {
+				if (!ball.isDeleted && ball.getPosition().x > window.innerHeight - ball.getDimension().h) {
+					ball.remove();
+				}
+			});
+		}, 1000 / 60);
+	};
+}
+var animate = new Animate();
+animate.start(600);
+animate.removeBall();
---
+++ JS/JS Assignment 6/ball.js
@@ -0,0 +1,75 @@
+function Position(width, height) {
+	//get arguements (width, height) ie (maxy,maxx)-coordinates of the area where we want to plot
+	var x = null;
+	var y = null;
+
+	this.generateXY = function() {
+		this.x = Math.random() * height;
+		this.y = Math.random() * width;
+	};
+}
+
+function Ball(i) {
+	var id = i;
+	var ball = null;
+	var interval = null;
+	this.isDeleteable = false;
+	this.isDeleted = false;
+	var that = this;
+
+	this.create = function() {
+		this.ball = document.createElement('div');
+		this.ball.id = i;
+        this.ball.style.borderRadius = '50%';
+        colors = ['red','blue','yellow','white','purple','green','orange','olive','pink'];
+		this.ball.style.backgroundColor = colors[parseInt(Math.random() * colors.length)];
+		this.ball.style.position = 'absolute';
+		document.body.appendChild(this.ball);
+
+		this.ball.addEventListener('click', function() {
+			if (that.isDeleteable) {
+				that.remove();
+			}
+		});
+	};
+
+	this.setDimension = function(width, height) {
+		this.ball.style.width = width + 'px';
+		this.ball.style.height = height + 'px';
+	};
+
+	this.getDimension = function() {
+		return {
+			h: parseInt(this.ball.style.height),
+			w: parseInt(this.ball.style.width)
+		};
+	};
+
+	this.setPosition = function(x, y) {
+		this.ball.style.top = x + 'px';
+		this.ball.style.left = y + 'px';
+	};
+
+	this.getPosition = function() {
+		return {
+			x: parseInt(this.ball.style.top),
+			y: parseInt(this.ball.style.left)
+		};
+	};
+
+	this.move = function() {
+		this.isDeleteable = true;
+		var rand = Math.random();
+		this.interval = setInterval(() => {
+			var currentTop = parseInt(that.ball.style.top);
+			var nextTop = currentTop + (rand * 200) % 3 + 1;
+			that.ball.style.top = nextTop + 'px';
+		}, 1000 / 60);
+	};
+
+	this.remove = function() {
+		document.body.removeChild(this.ball);
+		clearInterval(interval);
+		this.isDeleted = true;
+	};
+}
